caf54cb2e46e2a2346ec3c454a345fd2332806a1
drachelehre <klun.jeffrey@gmail.com>
2024-06-20 23:04:55 -0500

    something to fix

--- game_system.py
+++ game_system.py
@@ -26,9 +26,9 @@ class DungeonPath:
         super().__init__()
 
     def roll(self, die):
-        hit = int(random.random(1, 20))
+        #hit = int(random.random(1, 20))
         if die not in dice_list:
             raise ValueError('Error: Not a valid die')
-        damage = int(random.random(1, dice_list[die]))
-        return hit, damage
+        #damage = int(random.random(1, dice_list[die]))
+        #return hit, damage
 
